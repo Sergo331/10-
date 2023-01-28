fba1f616bae30e26168b22f139178ef4a49fa3b8
Sergo331 <proha331@gmail.com>
2023-01-28 21:24:20 +0300

    10 переменных
    10 переменных

---
+++ задание 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задание_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int moneyOnAccount;
+            string maneOfDog;
+            char firstLetter;
+            bool canIOpenDoor;
+            float result;
+            double input;
+            byte age;
+            long distanceToMoon;
+            short scoreOnfinish;
+            uint maxSpeed;
+        }
+    }
+}
